100e3be7d90f39729d1f026ba3b18fedd97ad1c7
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2017-08-15 12:27:27 -0700

    Rebase from 'upstream'

--- test/test_roscpp/test/src/timer_callbacks.cpp
+++ test/test_roscpp/test/src/timer_callbacks.cpp
@@ -74,15 +74,16 @@ class SteadyTimerHelper
     void callback(const SteadyTimerEvent& e)
     {
       bool first = last_call_.isZero();
-      SteadyTime last_call = last_call_;
-      last_call_ = SteadyTime::now();
-      SteadyTime start = last_call_;
+      last_call_ = e.current_real;
 
       if (!first)
       {
-        if (fabsf(expected_next_call_.toSec() - start.toSec()) > 0.1f)
+        double time_error = e.current_real.toSec() - e.current_expected.toSec();
+        // Strict check if called early, loose check if called late.
+        // Yes, this is very loose, but must pass in high-load, containerized/virtualized, contentious environments.
+        if (time_error > 5.0 || time_error < -0.01)
         {
-          ROS_ERROR("Call came at wrong time (%f vs. %f)", expected_next_call_.toSec(), start.toSec());
+          ROS_ERROR("Call came at wrong time (expected: %f, actual %f)", e.current_expected.toSec(), e.current_real.toSec());
           failed_ = true;
         }
       }
@@ -122,28 +123,12 @@ class SteadyTimerHelper
           setPeriod(p, true);
         }
       }
-      else
-      {
-        expected_next_call_ = e.current_expected + expected_period_;
-      }
-
-      SteadyTime end = SteadyTime::now();
-      last_duration_ = end - start;
 
       ++total_calls_;
     }
 
     void setPeriod(const WallDuration p, bool reset=false)
     {
-      if(reset)
-      {
-        expected_next_call_ = SteadyTime::now() + p;
-      }
-      else
-      {
-        expected_next_call_ = last_call_ + p;
-      }
-
       timer_.setPeriod(p, reset);
       expected_period_ = p;
     }
@@ -156,9 +141,7 @@ class SteadyTimerHelper
     }
 
     SteadyTime last_call_;
-    SteadyTime expected_next_call_;
     WallDuration expected_period_;
-    WallDuration last_duration_;
 
     bool failed_;
 
@@ -384,15 +367,16 @@ public:
   void callback(const WallTimerEvent& e)
   {
     bool first = last_call_.isZero();
-    WallTime last_call = last_call_;
-    last_call_ = WallTime::now();
-    WallTime start = last_call_;
+    last_call_ = e.current_real;
 
     if (!first)
     {
-      if (fabsf(expected_next_call_.toSec() - start.toSec()) > 0.1f)
+      double time_error = e.current_real.toSec() - e.current_expected.toSec();
+      // Strict check if called early, loose check if called late.
+      // Yes, this is very loose, but must pass in high-load, containerized/virtualized, contentious environments.
+      if (time_error > 5.0 || time_error < -0.01)
       {
-        ROS_ERROR("Call came at wrong time (%f vs. %f)", expected_next_call_.toSec(), start.toSec());
+        ROS_ERROR("Call came at wrong time (expected: %f, actual %f)", e.current_expected.toSec(), e.current_real.toSec());
         failed_ = true;
       }
     }
@@ -432,28 +416,12 @@ public:
         setPeriod(p, true);
       }
     }
-    else
-    { 
-      expected_next_call_ = e.current_expected + expected_period_;
-    }
-
-    WallTime end = WallTime::now();
-    last_duration_ = end - start;
 
     ++total_calls_;
   }
 
   void setPeriod(const WallDuration p, bool reset=false)
   {
-    if(reset)
-    {
-      expected_next_call_ = WallTime::now() + p;
-    }
-    else
-    {
-      expected_next_call_ = last_call_ + p;
-    }
-    
     timer_.setPeriod(p, reset);
     expected_period_ = p;
   }
@@ -466,9 +434,7 @@ public:
   }
 
   WallTime last_call_;
-  WallTime expected_next_call_;
   WallDuration expected_period_;
-  WallDuration last_duration_;
 
   bool failed_;
 
